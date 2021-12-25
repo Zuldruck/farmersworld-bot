7ca9d87a4c6fb4bd7f387b2f637d7bb892d01ba8
Lucas Sanchez <lucas.Sanchez@epitech.eu>
2021-12-25 21:22:57 +0100

    fix(environment): parse some variables as numbers instead of strings

--- src/environment.js
+++ src/environment.js
@@ -2,15 +2,15 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const REPAIR_IF_DURABILITY_LOWER = process.env.REPAIR_IF_DURABILITY_LOWER;
-export const RECOVER_IF_ENERGY_LOWER = process.env.RECOVER_IF_ENERGY_LOWER;
-export const LOWEST_ENERGY = process.env.LOWEST_ENERGY;
-export const MINIMUM_FEE = process.env.MINIMUM_FEE;
-export const MINIMUN_WITHDRAW = process.env.MINIMUN_WITHDRAW;
+export const REPAIR_IF_DURABILITY_LOWER = parseInt(process.env.REPAIR_IF_DURABILITY_LOWER);
+export const RECOVER_IF_ENERGY_LOWER = parseInt(process.env.RECOVER_IF_ENERGY_LOWER);
+export const LOWEST_ENERGY = parseInt(process.env.LOWEST_ENERGY);
+export const MINIMUM_FEE = parseInt(process.env.MINIMUM_FEE);
+export const MINIMUN_WITHDRAW = parseInt(process.env.MINIMUN_WITHDRAW);
 export const WITHDRAWABLE = process.env.WITHDRAWABLE.split(',');
 export const PAYBW = process.env.PAYBW;
 
 export const WALLET = process.env.WALLET;
 export const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
-export const PORT = process.env.PORT;
+export const PORT = parseInt(process.env.PORT);
